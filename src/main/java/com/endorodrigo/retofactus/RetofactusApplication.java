685ea3804ceeb8457ae15fd3e43b8223b3f3174e
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2025-01-21 21:51:46 -0500

    feat: creacion de la estructuta del proyecto

---
+++ src/main/java/com/endorodrigo/retofactus/RetofactusApplication.java
@@ -0,0 +1,13 @@
+package com.endorodrigo.retofactus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetofactusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetofactusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endorodrigo/retofactus/RetofactusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endorodrigo.retofactus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetofactusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
